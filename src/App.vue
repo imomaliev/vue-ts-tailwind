262e21b1f908056ee532f84e586b934bf9a3da39
Sardorbek Imomaliev <imomaliev@hey.com>
2021-08-01 17:20:08 +0700

    replace -webkit-font-smoothing and -moz-osx-font-smoothing with antialiased utily class

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,6 @@ export default defineComponent({
 
 <style>
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
