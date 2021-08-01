9123eb00a3468a1459d0bee79c00da3dab7f8b16
Sardorbek Imomaliev <imomaliev@hey.com>
2021-08-01 13:40:45 +0700

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
