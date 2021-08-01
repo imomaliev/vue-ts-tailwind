7605b6e023b0555d8c13cb1bad54f34046d6c736
Sardorbek Imomaliev <imomaliev@hey.com>
2021-08-01 16:49:25 +0700

    set font as a part of a tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
